29a608f195223b3f6f5bd2f5a1a3c11f842240bb
BrentVanwildemeersch <brent.vanwildemeersch@ocular.be>
2020-05-22 21:54:14 +0200

    Tests + first project

---
+++ tests/test_3.py
@@ -0,0 +1,4 @@
+print('My name is')
+name = input()
+for i in range(len(name)):
+    print('Jimmy five times ('+str(i) + ')')
---
+++ tests/test_4.py
@@ -0,0 +1,8 @@
+def printMyName(myName):
+    print('My name is' + myName)
+
+
+print('Who are you ?')
+myName = input()
+
+printMyName(myName)
